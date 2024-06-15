8b702ca8b8edbd2add137139ca2011a3ff7a99d4
Mohammed Fuad <mohammedfuadalnashriy@gmail.com>
2024-06-15 15:55:40 +0800

    feat: created the tasks views & update tasks serializer

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+# Create a new task
+class TaskCreateView(generics.CreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+# Retrieve list of tasks
+class TaskListView(generics.ListAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+# Delete a task
+class TaskDeleteView(generics.DestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
